5c5c5fa400be67528ff5cc560e50a7a5c5756e5b
Justin Bachus <justin@bachus.us>
2023-06-07 16:28:07 -0600

    Initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+# Greenplug AWS Lambda Function
+This function monitors the [Northwestern Energy Montana generation stats](https://www.northwesternenergy.com/clean-energy/where-does-your-energy-come-from/electric-generation) to determine whether there is excess green energy being generated from wind and solar.  It updates a variable on [SEQUEmatic](https://www.sequematic.com) that allows users to subscribe to the sequences and link their smart plugs from Tuya, Sonoff, or Philips hue.
+
+## Use Case
+Many of us have electrical appliances that don't require power all the time.  Many electric utilities also have a high volume of intermittent generating sources like wind or solar that are weather dependent.  Shifting this electrical load to times when green energy is abundant reduces the carbon footprint of these appliances and has the added benfits of reducing downstream electrical generation costs by utilizing power that is essentially free.
+
+## Usage
+To use these sequences, sign up for an account at [SEQUEmatic](https://www.sequematic.com).  Once you have [linked services](https://sequematic.com/services) with your smart plugs, you can import the following sequences and update the triggers for your own smart plugs:
+- [Turn on switch when there is excess green energy in Montana](https://sequematic.com/import-sequence/21377)
+- [Turn off switch when there is not excess green energy in Montana](https://sequematic.com/import-sequence/21378)
---
+++ handler.py
@@ -0,0 +1,95 @@
+import requests
+import os
+from dotenv import load_dotenv
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += 'DEFAULT:!DH'
+load_dotenv()
+
+default_green_energy_threshold = 80
+if os.getenv('GREEN_ENERGY_THRESHOLD'):
+  try:
+    green_energy_threshold = int(os.getenv('GREEN_ENERGY_THRESHOLD'))
+    if green_energy_threshold < 0 or green_energy_threshold > 100:
+      raise
+  except:
+    print("GREEN_ENERGY_THRESHOLD must be an integer between 0 and 100.  Default: 80")
+    exit(1)
+else:
+  green_energy_threshold = default_green_energy_threshold
+
+if os.getenv('SEQUEMATIC_URL_SUFFIX'):
+  sequematic_url_suffix = os.getenv('SEQUEMATIC_URL_SUFFIX')
+else:
+  print("The environment variable SEQUEMATIC_URL_SUFFIX must be set.  Example: 9999/ABCDF0123/variable_name")
+  exit(1)
+
+fields = ['LblReadDate',
+          'LblWindData',
+          'LblHydroData',
+          'LblThermData',
+          'LblSolarData',
+          'LblForecastData',
+          'LblTotalData']
+
+def run(event, context):
+  #TODO: Make http requests async
+  url = 'https://www.northwesternenergy.com/get-electricity-generation'
+  response = requests.get(url)
+  data = response.json().get('Data')
+  parsed_data = {}
+
+  min_datapoints = len(data.get(fields[0]).split(','))-1
+  for field in fields:
+    parsed_data[field] = data.get(field).split(',')
+    if len(parsed_data[field]) < min_datapoints:
+      min_datapoints = len(parsed_data[field])-1
+
+  green_energy = int(parsed_data['LblWindData'][min_datapoints]) + \
+                int(parsed_data['LblHydroData'][min_datapoints]) + \
+                int(parsed_data['LblSolarData'][min_datapoints])
+  dirty_energy = int(parsed_data['LblThermData'][min_datapoints])
+  generation = green_energy + dirty_energy
+  consumption = int(parsed_data['LblForecastData'][min_datapoints])
+
+  #TODO: Plot these as CloudWatch metrics
+  print(f"Date: {parsed_data['LblReadDate'][min_datapoints]}")
+  print(f"Green: {green_energy}")
+  print(f"Dirty: {dirty_energy}")
+  print(f"Production: {generation}")
+  print(f"Consumption: {consumption}")
+  print(f"Green Pct Generation: {(green_energy/generation):.0%}")
+  print(f"Green Pct Consumption: {(green_energy/consumption):.0%}")
+  print(f"Green Energy Threshold: {green_energy_threshold}%")
+
+  # Our rule is to turn on switch when green energy exceeds the threshold
+  # and generation exceeds consumption
+
+  switch_desired_on = False
+  green_energy_pct = int(round((green_energy/consumption)*100,0))
+  if generation > consumption and green_energy_pct >= green_energy_threshold:
+    print("Recommend Switch ON")
+    switch_desired_on = True
+  else:
+    print("Recommend switch OFF")
+
+  switch_is_on = False
+  try:
+    current_status_req = requests.get(f"https://sequematic.com/variable-get/{sequematic_url_suffix}")
+    switch_is_on = bool(int(current_status_req.text))
+    if switch_is_on:
+      print("Switch is currently ON")
+    else:
+      print("Switch is currently OFF")
+  except:
+    print("Unable to get switch status")
+    return {"statusCode": 500}
+
+  if switch_is_on is not switch_desired_on:
+    if switch_desired_on:
+      print("Turning switch ON")
+      webhook_req = requests.get(f"https://sequematic.com/variable-change/{sequematic_url_suffix}/=1")
+    else:
+      print("Turning switch OFF")
+      webhook_req = requests.get(f"https://sequematic.com/variable-change/{sequematic_url_suffix}/=0")
+    if webhook_req.status_code == 200:
+      print("Successfully notified sequematic webhook")
+      return {"statusCode": 200}
